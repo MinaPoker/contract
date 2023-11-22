7a0337dd02940a1d806607eccdccd876b2885b9c
ks <101751723+0xKryptonian@users.noreply.github.com>
2023-11-22 16:09:07 +0530

    card validation for user

--- src/card_verifier/sequencer.ts
+++ src/card_verifier/sequencer.ts
@@ -183,3 +183,144 @@ function MerkleMapExtended<
 }
 
 const PORT = 3000;
+
+
+function Voting({
+    proposalTitle,
+    proposalId,
+    feePayer,
+    electionPeriod,
+    challengingPeriod,
+    voterData,
+}: {
+    proposalTitle: string;
+    proposalId: string;
+    feePayer: PrivateKey;
+    electionPeriod: {
+        start: UInt32;
+        end: UInt32;
+    };
+    challengingPeriod: {
+        start: UInt32;
+        end: UInt32;
+    };
+    voterData: { publicKey: PublicKey; weight: Field }[];
+}) {
+    let acceptingVotes = true;
+
+    const VoterDataTree = MerkleMapExtended<VoterData>();
+
+    // pretty naive but will do for now
+    voterData.forEach((voter) => {
+        // voters are indexed by the hash of their public key
+        VoterDataTree.set(
+            Poseidon.hash(voter.publicKey.toFields()),
+            new VoterData(voter)
+        );
+    });
+
+    const NullifierTree = new MerkleMap();
+
+    // this will serve as our mem pool, as we don't have to aggregate all votes directly
+    const VotePool: Vote[] = [];
+
+    return {
+        async deploy() { },
+        async listen() {
+            const app = express();
+            app.use(express.json());
+
+            app.post('/castVote', (req, res) => {
+                if (!acceptingVotes) {
+                    res.status(400).json({
+                        reason: 'Voting period is over - not accepting more votes.',
+                    });
+                }
+                console.debug('received a vote');
+                let jsonVote = req.body;
+                if (validateJSONVote(jsonVote)) {
+                    let isValidOrError = validateVote(jsonVote, VotePool, voterData);
+                    if (typeof isValidOrError === 'undefined') {
+                        res.status(200).send();
+                    } else {
+                        res.status(400).json({
+                            reason: isValidOrError as string,
+                            data: jsonVote,
+                        });
+                    }
+                } else {
+                    res.status(400).json({
+                        reason: 'Invalid vote structure.',
+                        data: jsonVote,
+                    });
+                }
+            });
+
+            app.post('/proposal', (req, res) => {
+                res.status(200).json({
+                    proposalTitle,
+                    proposalId,
+                });
+            });
+
+            app.post('/tree', (req, res) => {
+                res.status(200).json(VoterDataTree.flat());
+            });
+
+            // this triggers tallying but just for now
+            app.post('tally', (req, res) => {
+                acceptingVotes = false;
+                // TODO
+            });
+
+            app.listen(PORT, () => {
+                console.log(
+                    `[server]: Vote sequencer running at http://localhost:${PORT}`
+                );
+            });
+        },
+    };
+}
+
+function validateVote(
+    jsonVote: JSONVote,
+    votePool: Vote[],
+    voterData: { publicKey: PublicKey }[]
+): undefined | string {
+    let vote = Vote.fromJSON(jsonVote);
+
+    let isVoter = voterData.find((v) =>
+        v.publicKey.equals(vote.voter).toBoolean()
+    );
+
+    if (!isVoter) {
+        return 'Voter is not part of list of eligible voters.';
+    }
+
+    if (!vote.yes.add(vote.no).add(vote.abstained).equals(1).toBoolean()) {
+        return 'You can only cast one vote!';
+    }
+
+    let payload = vote.voter
+        .toFields()
+        .concat([vote.yes, vote.no, vote.abstained, vote.voterDataRoot]);
+    let isValid = vote.authorization.verify(vote.voter, payload).toBoolean();
+
+    if (isValid) {
+        // we check that there are no multiple votes from the same voter in the pool - just some pre-checking to prevent spam
+        let exists = votePool.find((v) => v.voter.equals(vote.voter));
+        if (exists) {
+            votePool.push(vote as Vote);
+            return;
+        }
+        return 'Vote already casted.';
+    }
+    return 'Vote is not valid.';
+}
+
+// can use this to schedule the tallying phase
+function runIn(ms: number, cb: () => void) {
+    setTimeout(cb, ms);
+}
+
+function aggregateVotes(votePool: Vote[]) { }
