170bfd1b36f7d536be0f541dd018553148d29bcf
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-11-25 14:49:41 +0530

    Import and re-export PokerGame from PokerContract.ts for modular access

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
+// Import the PokerGame class or object from the PokerContract.js file
 import { PokerGame } from './PokerContract.js';
 
+// Export the imported PokerGame for use in other parts of the application
 export { PokerGame };
-
