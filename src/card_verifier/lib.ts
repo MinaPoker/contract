cf66cb4e32f17a9014509fe3091175b284c72546
0xshikhar <101751723+0xKryptonian@users.noreply.github.com>
2023-12-16 17:25:46 +0530

    updated the nullifer in lib.ts

--- src/card_verifier/lib.ts
+++ src/card_verifier/lib.ts
@@ -1,32 +1,43 @@
 import { Field, PublicKey, Poseidon, MerkleMap } from 'o1js';
 import { Vote } from './sequencer';
 
-export { Nullifier, calculateNullifierRootTransition, calculateVotes };
+export { Nullifier, calculateNullifierRootTransition, calculateVotes, verifyNullifierRoot };
 
+// Calculate the nullifier hash for a Mina Poker game
 function Nullifier(publicKey: PublicKey, proposalId: Field) {
   return Poseidon.hash(publicKey.toFields().concat(proposalId));
 }
 
+// Calculate the nullifier root transition for Mina Poker
 function calculateNullifierRootTransition(
   nullifierTree: MerkleMap,
   votes: Vote[]
 ) {
-  let rootBefore = nullifierTree.getRoot();
+  // Get the root of the nullifier tree before processing votes
+  const rootBefore = nullifierTree.getRoot();
+
+  // Update the nullifier tree with each vote
   votes.forEach((v) => {
-    let key = Nullifier(v.voter, v.proposalId);
-    nullifierTree.set(key, Field(1));
+    const key = Nullifier(v.voter, v.proposalId);
+    nullifierTree.set(key, Field(1)); // Assuming Field(1) represents a vote
   });
+
+  // Get the root of the nullifier tree after processing votes
+  const rootAfter = nullifierTree.getRoot();
+
   return {
     rootBefore,
-    rootAfter: nullifierTree.getRoot(),
+    rootAfter,
   };
 }
 
+// Calculate the aggregated votes for Mina Poker
 function calculateVotes(votes: Vote[]) {
   let yes = Field(0);
   let no = Field(0);
   let abstained = Field(0);
 
+  // Aggregate votes
   votes.forEach((v) => {
     yes = yes.add(v.yes);
     no = no.add(v.no);
@@ -38,4 +49,10 @@ function calculateVotes(votes: Vote[]) {
     no,
     abstained,
   };
+}
+
+// Verify nullifier root after processing votes
+function verifyNullifierRoot(rootBefore: Field, rootAfter: Field): boolean {
+  // Check if the roots match
+  return rootBefore.equals(rootAfter).toBoolean();
 }
